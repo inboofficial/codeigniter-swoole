8c1f9c550f5cd9354c2d9a222f3dfc553c04820c
inbomank <mohammad.ank@outlook.com>
2021-10-04 17:05:02 +0330

    add symphony event

---
+++ src/event/InboListener.php
@@ -0,0 +1,41 @@
+<?php namespace inboir\CodeigniterS\event;
+
+
+use inboir\CodeigniterS\event\Events;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+abstract class InboListener implements EventSubscriberInterface
+{
+
+    public function __construct()
+    {
+        $this->register();
+    }
+
+    private static ?self $instance = null;
+
+    public static function getInstance(): InboListener
+    {
+        if (!(static::$instance instanceof static)) {
+            static::$instance = new static();
+        }
+        return static::$instance;
+    }
+
+    protected function register()
+    {
+        foreach ($this->getSubscribedEvents() as $eventName => $params) {
+            if (is_string($params)) {
+                Events::register($eventName, [$this, $params]);
+            } elseif (is_string($params[0])) {
+                Events::register($eventName, [$this, $params[0]], isset($params[1]) ? $params[1] : 0);
+            } else {
+                foreach ($params as $listener) {
+                    Events::register($eventName, [$this, $listener[0]], isset($listener[1]) ? $listener[1] : 0);
+                }
+            }
+        }
+    }
+
+    public abstract static function getSubscribedEvents(): array;
+}
