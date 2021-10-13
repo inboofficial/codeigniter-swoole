c06d59e65af3a891cdf135fd7dac33cc1d8f01bc
inbomank <mohammad.ank@outlook.com>
2021-10-13 10:47:37 +0330

    change event model

--- src/event/Event.php
+++ src/event/Event.php
@@ -4,11 +4,30 @@
 namespace inboir\CodeigniterS\event;
 
 
+
 class Event
 {
-    public string $event_rout = 'rout.event';
+    protected string $event_rout = 'rout.event';
     public function getEventRout():string
     {
         return $this->event_rout;
     }
+    protected array $eventData;
+
+    /**
+     * @return array
+     */
+    public function getEventData(): array
+    {
+        return $this->eventData;
+    }
+
+    /**
+     * @param array $eventData
+     */
+    public function setEventData(array $eventData): void
+    {
+        $this->eventData = $eventData;
+    }
+
 }
