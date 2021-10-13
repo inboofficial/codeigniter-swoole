1a501e48357416b4cdf929dd7b9edd4f25d6b39c
inbomank <mohammad.ank@outlook.com>
2021-10-13 10:39:34 +0330

    more refactor

--- src/event/Event.php
+++ src/event/Event.php
@@ -6,7 +6,7 @@ namespace inboir\CodeigniterS\event;
 
 class Event
 {
-    protected string $event_rout = 'rout.event';
+    public string $event_rout = 'rout.event';
     public function getEventRout():string
     {
         return $this->event_rout;
